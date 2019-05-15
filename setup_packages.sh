85450cb7700c0d1742957235cded4419dd074b06
naohisas <naohisa.sakamoto@gmail.com>
2019-05-15 16:59:08 +0000

    first import.

---
+++ setup_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt-get clean && sudo apt-get update
+sudo apt-get install emacs
+sudo apt-get install build-essential
+sudo apt-get install pkg-config
+sudo apt-get install cmake
+sudo apt-get install autoconf
+sudo apt install libtool
+sudo apt install flex
+sudo apt install bison
+sudo apt-get install openmpi-doc openmpi-bin libopenmpi-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install libexpat1-dev
+sudo apt-get install python
+sudo apt-get install python-pip
+pip install numpy
+pip install scipy
+
